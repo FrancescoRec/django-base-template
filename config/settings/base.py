f5cc6161109165a0d001fde1fcfeabde21b3f636
francesco <francesco@letsgosky.com>
2025-12-05 15:09:30 +0100

    ref(myproject): fixed logout

--- config/settings/base.py
+++ config/settings/base.py
@@ -113,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'it-it'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Rome'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
